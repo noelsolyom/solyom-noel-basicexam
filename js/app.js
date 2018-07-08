ded5e8010d5525d2a2e58abd720bb85d4b505757
Solyom Noel <solyom.noel@gmail.com>
2018-07-08 13:37:35 +0200

    FUNCTION: overwrite value of object property

--- js/app.js
+++ js/app.js
@@ -8,13 +8,13 @@
 // Visszatérési érték 1, ha létezik a keresett tulajdonság.
 // A visszatérési érték 0, ha a paraméterként nem objektumot, vagy null-t adtunk át.
 // A visszatérési érték -1, ha nem létezik a keresett tulajdonság.
-function chechkIfObjectHasOwnProprty(inpuObject, propertyValue) {
+function chechkIfObjectHasOwnProprty(inpuObject, propertyName) {
   // Visszatérési érték definiállása és inicializálása. Feltételezzük, hogy az objektum rendelkezik a keresett tulajdonsággal.
   var valueToReturn = 1;
   // Megvizsgáljuk, hogy a paraméterként átadott objetum valóban objektum-e ÉS az nem null.
   if ((typeof inpuObject === 'object') && (inpuObject !== null)) {
     // Megvizsgáljuk, hogy a paraméterként átadott objektumnak létezik e a keresett tulajdonsága.
-    if (!inpuObject.hasOwnProperty(propertyValue)) {
+    if (!inpuObject.hasOwnProperty(propertyName)) {
     // Amennyiben a paraméterként átadott objektumnak nincs a keresett tulajdonsága, a visszatérési értéket -1-re állítjuk
       valueToReturn = -1;
     }
@@ -22,6 +22,18 @@ function chechkIfObjectHasOwnProprty(inpuObject, propertyValue) {
   } else {valueToReturn = 0;}
   // A függvény visszaadja a visszatérési értéket.
   return valueToReturn;
+}
+
+// Függvény, ami felülírja a paraméterként átadott objektum adott tulajdonságának értékét.
+// Paraméter: objektum, tulajdonság, új érték.
+// MEGJEGYZÉS: sokkal elegánsabb lenne, ha az objektum prototípusánál vennénk fel új metódusként az alábbi függvényt.
+function overwriteValueOfObjectProprty(inpuObject, propertyName, newValue); {
+  // Megvizsgáljuk, hogy a paraméterként kapott objektum rendelkezik e a módosítandó tulajdonsággal. (Máskülömben új)
+  // tulajdonság jönne létre az adott objektumban.
+  if (chechkIfObjectHasOwnProprty(inpuObject, propertyName)) {
+    // A paraméterként kapott objektum adott tulajdonságának értékét módosítjuk az új értékre.
+    inpuObject[`${propertyName}`] = newValue;
+  }
 }
 
 
@@ -40,6 +52,10 @@ function successAjax(xhttp) {
   // Innen lesz elérhető a JSON file tartalma, tehát az adatok amikkel dolgoznod kell
   var userDatas = JSON.parse(xhttp.responseText);
   // Innen lehet hívni.
-  console.log(userDatas);
+  
+
+
+
+
 }
 getData('/json/spaceships.json', successAjax);
